88818d4d06d3b284269bdcd92fff2f7b325b5f8f
Washington Meneses <wmeneses@vannon.com.br>
2020-02-28 14:01:09 -0300

    SSR e arquivo Leia-ME

---
+++ App/src/pipes/env/image-url.pipe.ts
@@ -0,0 +1,30 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import * as enviroment from "src/environments/environment";
+import { DomSanitizer } from '@angular/platform-browser';
+@Pipe({
+    name: 'imageUrl'
+})
+export class ImageUrlPipe implements PipeTransform {
+    constructor(protected sanitizer: DomSanitizer) {}
+
+    transform(imagePath: any, type: any = null) {
+
+        let path = enviroment.environment.imagesServerUrl + imagePath;
+            let resource = null;
+        switch (type) {
+            case ('style'):
+            resource =  this.sanitizer.bypassSecurityTrustStyle(path);
+            break;
+            case ('none'):
+                resource =  path;
+                break;
+            default:
+            resource = this.sanitizer.bypassSecurityTrustResourceUrl(path);
+            break;
+        }
+
+        return resource;
+
+    }
+
+}
